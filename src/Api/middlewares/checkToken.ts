7da79fdfb18f8cc3a10cabe2171f43b3de0ae239
Lucas Machado <lukaasmachado143@gmail.com>
2023-11-30 01:23:17 -0300

    feat: created middleware for chack token.

---
+++ src/Api/middlewares/checkToken.ts
@@ -0,0 +1,34 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { verify } from "jsonwebtoken";
+import { GeneralResponse } from "../../Core/@types/GeneralResponse";
+
+export function CheckToken(
+  request: FastifyRequest,
+  reply: FastifyReply,
+  done: any
+) {
+  const response: GeneralResponse = {
+    message: "",
+    success: false,
+  };
+  const recivedToken = request.headers.authorization;
+  if (!recivedToken) {
+    response.message = "Token pendente !";
+    return reply.code(401).send(response);
+  }
+
+  const secretKey = process.env.SECRET_KEY;
+  if (!secretKey) {
+    response.message = "SRCRET_KEY pendente !";
+    return reply.code(401).send(response);
+  }
+
+  const [, token] = recivedToken.split(" ");
+  try {
+    verify(token, secretKey);
+    done();
+  } catch (error) {
+    response.message = "Token inválido !";
+    return reply.code(401).send(response);
+  }
+}
