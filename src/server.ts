fc8560f60579e2c170a34bb11d135e208f553ded
Lucas Machado <lukaasmachado143@gmail.com>
2024-02-27 23:15:28 -0300

    vercel.json v12

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
-// import { app } from "./app";
-// app.listen({ port: 3000 }, (err, address) => {
-//   if (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-//   console.log(`Server listening at ${address}`);
-// });
+import fastify, { FastifyInstance } from "fastify";
+import { UserRoutes } from "./Api/Routes/UserRoutes";
+import fastifyCors from "@fastify/cors";
+import { TransferRoutes } from "./Api/Routes/TransferRoutes";
+import multipart from "@fastify/multipart";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import {
+  fastifySwaggeroptions,
+  fastifySwaggerUIOptions,
+} from "./ExternalConfigs/swaggerConfigs";
+
+const app: FastifyInstance = fastify();
+
+app.register(fastifyCors);
+app.register(fastifySwagger as any, fastifySwaggeroptions);
+app.register(fastifySwaggerUI as any, fastifySwaggerUIOptions);
+app.register(multipart);
+app.register(UserRoutes, { prefix: "/user" });
+app.register(TransferRoutes, { prefix: "/transfer" });
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
+// export { app };
